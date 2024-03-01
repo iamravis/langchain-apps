bd09d4abf32a2a6fdad10ca33b779cc638d1dc48
rsps1131 <rsps1131@gmail.com>
2024-03-01 01:05:13 +0000

    hyperparameters tuning

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import setup_logging
@@ -23,7 +24,7 @@ def save_object(file_path, obj):
  
  
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     ''' 
         This function takes train and test data 
         along with models and outputs the model
@@ -34,11 +35,15 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         report = {}       
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            
-            logger.info(f"Fitting {i} - {model} ")
-            
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             
+            logger.info(f"Fitting {i} - {model} ")
             y_train_pred = model.predict(X_train)
             
             y_test_pred = model.predict(X_test)
